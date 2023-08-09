6a6e28fe2b342a62cde1f8962e6cd56d19e20c4a
abbydoag <86386562+abbydoag@users.noreply.github.com>
2023-08-09 13:35:31 -0600

    Add files via upload

---
+++ Lab2B.c
@@ -0,0 +1,22 @@
+/*
+*----------------------------------------
+* Lab2.c
+* ---------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3086 - Programacion de Microprocesadores
+* Laboratorio No.2
+* Abby Donis y Andrés Chivalan
+* ---------------------------------------
+*
+* Programa que realice producto cruz de 
+* dos vectores. 
+*
+*----------------------------------------*/
+#define N 3 
+main(){
+    int N;
+    
+
+    
+
+}
